57472ade653a6f3d50ab07d57bd825c04a3f4c7b
Vildan Beyza Çetin <vcetin1356@gmail.com>
2023-12-07 15:00:40 +0300

    input value taken

--- src/components/cardContainer/CardContainer.jsx
+++ src/components/cardContainer/CardContainer.jsx
@@ -7,12 +7,12 @@ import { Container, Row, Col } from "react-bootstrap";
 const CardContainer = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [toggle,setToggle]=useState(false)
 
-  const handleSwap=()=>{
-    setToggle(!toggle)
-    console.log(toggle)
-  }
+  const [input,setInput]=useState("")
+console.log(input)
+
+const filteredCard= data.filter((card)=>(card.name.toLowerCase().includes(input.toLowerCase())))
+console.log(filteredCard)
   useEffect(() => {
     const fetcData = async () => {
       try {
@@ -33,20 +33,24 @@ const CardContainer = () => {
       {loading ? (
         <p>Loading...</p>
       ) : (
+       
         <Container>
-           <input type="text" className={"mx-auto d-flex"} placeholder="Search the character..." size={30}/>
+           <input type="text" className={"mx-auto d-flex"} placeholder="Search the character..." size={30} onChange={(e)=>setInput(e.target.value)} value={input}/>
+
           <Row xs={1} md={3} className="g-4" >
             {
-              data.filter((char)=>(
+              input==="" ? (
+                data.filter((char)=>(
                 char.image !== ""
               )).map((character) => (
-                // <div onClick={handleSwap}>
-                //   {toggle 
-                //   ? <ToggleCard key={character.id} {...character}/> 
-                //   : <Character key={character.id} {...character}/>  }
-                // </div>
                 <Character key={character.id} {...character}/>
               ))
+              ) : (
+                filteredCard.filter((card)=>(
+                  card.image!==""
+                )).map((card)=>(<Character key={card.id} {...card}/>))
+              )
+              
             }
           </Row>
         </Container>
