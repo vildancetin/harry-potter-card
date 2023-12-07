fc56b8113ca24badb412d578e08aa17e6628ed0f
Vildan Beyza Çetin <vcetin1356@gmail.com>
2023-12-07 16:00:48 +0300

    list-item color added

--- src/components/character/Character.jsx
+++ src/components/character/Character.jsx
@@ -3,7 +3,7 @@ import CharacterStyle from "./Character.module.css";
 import { Card, Col, ListGroup } from "react-bootstrap";
 export const Character = (props) => {
   const [flip, setFlipped] = useState(false);
-  const { name, image, id } = props;
+  const { name, image, id,house } = props;
   return (
     <div
       onClick={() => {
@@ -15,7 +15,15 @@ export const Character = (props) => {
       ) : (
         <Card style={{ width: "18rem", height: "30rem" } } className={CharacterStyle.card}>
           <Card.Img src={image} alt="..." style={{ height: "25rem" }} />
-          <Card.Body>
+          <Card.Body className={`${
+          house === "Slytherin"
+            ? CharacterStyle["title-sly"]
+            : house === "Gryffindor"
+            ? CharacterStyle["title-gry"]
+            : house === "Hufflepuff"
+            ? CharacterStyle["title-huf"]
+            : CharacterStyle["title-rav"]
+          }`}>
             <Card.Text className={CharacterStyle.name}>{name} </Card.Text>
           </Card.Body>
         </Card>
@@ -37,14 +45,38 @@ export const ToggleCard = ({ species, house, patronus, image, name }) => {
             : house === "Hufflepuff"
             ? CharacterStyle["title-huf"]
             : CharacterStyle["title-rav"]
-        }`}
+        } text-center`}
       >
-        <Card.Title>{name.toUpperCase()}</Card.Title>
+        <Card.Title className={CharacterStyle.toggleName}>{name.toUpperCase()}</Card.Title>
       </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>{house}</ListGroup.Item>
-        <ListGroup.Item>{species}</ListGroup.Item>
-        <ListGroup.Item>{patronus}</ListGroup.Item>
+      <ListGroup  className={`${CharacterStyle.list} list-group-flush`}>
+        <ListGroup.Item className={`${
+          house === "Slytherin"
+            ? CharacterStyle["listItem-sly"]
+            : house === "Gryffindor"
+            ? CharacterStyle["listItem-gry"]
+            : house === "Hufflepuff"
+            ? CharacterStyle["listItem-huf"]
+            : CharacterStyle["title-rav"]
+          }`} >{house}</ListGroup.Item>
+        <ListGroup.Item className={`${
+          house === "Slytherin"
+            ? CharacterStyle["listItem-sly"]
+            : house === "Gryffindor"
+            ? CharacterStyle["listItem-gry"]
+            : house === "Hufflepuff"
+            ? CharacterStyle["listItem-huf"]
+            : CharacterStyle["listItem-rav"]
+          }`}>{species}</ListGroup.Item>
+        <ListGroup.Item className={`${
+          house === "Slytherin"
+            ? CharacterStyle["listItem-sly"]
+            : house === "Gryffindor"
+            ? CharacterStyle["listItem-gry"]
+            : house === "Hufflepuff"
+            ? CharacterStyle["listItem-huf"]
+            : CharacterStyle["listItem-rav"]
+          }`}>{patronus}</ListGroup.Item>
       </ListGroup>
     </Card>
   );
